dd0d984cfb763f0e4742d541ad3ef41adb10b082
TabataLFOliveira <tabata.l.freire@gmail.com>
2026-02-26 16:47:17 -0300

    atualização 3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 let nome = "Tabata"
 console.log(nome)
+
+let nomeDoPai = "Renan"
+let idadeDoPai = "33"
+let nomeDaMae = "Tabata"
+let idadeDaMae = "30"
+console.log("O nome do pai é " + nomeDoPai + " e ele tem " + idadeDoPai + " anos.")
+console.log("O nome da mãe é " + nomeDaMae + " e ela tem " + idadeDaMae + " anos.")
+
+let nomeDoFilho = "Filipe"
+let idadeDoFilho = "1"
+console.log("O nome do filho é " + nomeDoFilho + " e ele tem " + idadeDoFilho + " anos.")
+
+console.log(nomeDoFilho + " é filho de " + nomeDoPai + " e " + nomeDaMae + ".")
